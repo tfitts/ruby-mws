78cd7accb5f1b89c290f69ad4e20e81cfea39750
Tyler Fitts <tfitts@gmail.com>
2015-09-16 13:41:29 -0600

    GetMyPriceForSKU added to products

--- lib/ruby-mws/api/products.rb
+++ lib/ruby-mws/api/products.rb
@@ -12,6 +12,17 @@ module MWS
                       :upc => "IdList.Id",
                       :ean => "IdList.Id"
                   }
+                  
+      def_request [:get_my_price_for_SKU],
+      :verb => :post,
+      :uri => '/Products',
+      :version => '2011-10-01',
+      :single_marketplace => true,
+      :lists => {
+          :skus => "SellerSKUList.SellerSKU"
+      }
+  
+
 
     end
 
